496c54162f8d2bee249b45746e76fde364fe73cf
Bitee-cd <65104898+Bitee-cd@users.noreply.github.com>
2022-03-24 01:22:33 +0100

    image alt

--- src/Navbar.js
+++ src/Navbar.js
@@ -17,10 +17,12 @@ function Navbar() {
     return (
         <div className={`nav ${show && 'nav__black'}`}>
             <img
+            alt="nil"
             className="nav__logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
             />
             <img
+            alt="nil"
             className="nav__avatar"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNsCijZ2c7h4d0Qv44cGVOe8Y8NYRj_pZq-Q&usqp=CAU"/>
 
